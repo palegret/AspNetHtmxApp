1e78e160fe4c3fc297b5cea401d633c50e67e7ce
Philip Alegret <philip.alegret@gmail.com>
2026-08-23 22:27:53 -0400

    Add htmx test code

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -3,10 +3,21 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AspNetHtmxApp.Pages;
 
+/**
+ * When working with htmx, the HX-Request header helps differentiate between 
+ * standard and htmx-triggered requests. You can check this in your backend 
+ * code to return different responses depending on whether the request 
+ * originated from htmx.
+ */
+
 public class IndexModel : PageModel
 {
-    public void OnGet()
-    {
+    bool IsHtmxRequest => Request.Headers.ContainsKey("HX-Request");
 
+    public IActionResult OnGetHello()
+    {        
+        return IsHtmxRequest 
+            ? Content("<strong>Hello, htmx!</strong>", "text/html") 
+            : Page();
     }
 }
