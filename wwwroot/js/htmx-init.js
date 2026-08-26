d03570ed942d453c1b70f3d2ca7e56f5fb160776
Philip Alegret <philip.alegret@gmail.com>
2026-08-26 13:27:52 -0400

    Move htmx init code to js/htmx-init.js

---
+++ wwwroot/js/htmx-init.js
@@ -0,0 +1,20 @@
+﻿((document, htmx) => {
+  document.body.addEventListener('htmx:beforeRequest', (event) => {
+    console.log('Request starting:', event.detail.pathInfo);
+  });
+
+  document.body.addEventListener('htmx:afterRequest', (event) => {
+    console.log('Request complete:', event.detail.xhr.status);
+  });
+
+  document.body.addEventListener('htmx:responseError', (event) => {
+    console.error('Request failed:', event.detail.xhr.status);
+  });
+
+  document.body.addEventListener('htmx:configRequest', (event) => {
+    const token = document.querySelector('input[name="__RequestVerificationToken"]');
+    token && event.detail.headers['RequestVerificationToken'] = token.value;
+  });
+
+  htmx.logAll();
+})(window.document, window.htmx);
