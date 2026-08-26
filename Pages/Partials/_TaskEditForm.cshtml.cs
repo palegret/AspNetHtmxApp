f613bf13c75460cf0419a68b81219e9d3e73f2f7
Philip Alegret <philip.alegret@gmail.com>
2026-08-26 13:01:53 -0400

    Add TaskItem partials

---
+++ Pages/Partials/_TaskEditForm.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspNetHtmxApp.Pages.Partials;
+
+public class TaskEditFormModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
---
+++ Pages/Partials/_TaskItem.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspNetHtmxApp.Pages.Partials
+{
+    public class _TaskItemModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
